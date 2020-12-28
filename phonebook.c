723f94eaeba53ebe95b40c0fad155a2289252120
Arya <63557848+Ary4-alt@users.noreply.github.com>
2020-12-28 13:36:41 -0500

    Create phonebook.c
    The=is program works as a digital phone book, in which the user can store the names and numbers of  their contacts. A CSV file gets updated in real time that can be also used in programs like Microsoft Excel. Also the CSV file gets created after compiling the project too and the printed contacts are accessible directly via the program too.

---
+++ phonebook.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main (void)
+
+{
+
+    //open file
+    FILE *file = fopen("phonebook.csv", "a");
+
+    //Get string from the user
+    char* name = get_string ("Name: ");
+    char* number = get_string ("Number: ");
+
+    //print (write) strings to file
+    fprintf( file, "%s, %s\n", name, number);
+
+    //close file
+    fclose (file);
+
+}
+
+//The csv file gets updated in real time and is compatable with microsoft excel.
